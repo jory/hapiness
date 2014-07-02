3bcf3a1a411f331ac273398ab878836ff4b16c22
Jory Graham <jory.graham@gmail.com>
2014-07-02 14:37:00 -0400

    DIRECTORIES (with 'path' safety!)

--- directories/program.js
+++ directories/program.js
@@ -1,12 +1,13 @@
 var Hapi = require('hapi');
+var path = require('path');
 
 var server = Hapi.createServer('localhost', Number(process.argv[2] || 8080));
 
 server.route({
   method: 'GET',
-  path: "/foo/bar/baz/{param}",
+  path: "/foo/bar/baz/{filename}",
   handler: {
-    directory: { path: './public' }
+    directory: { path: path.join(__dirname + '/public') }
   }
 });
 
